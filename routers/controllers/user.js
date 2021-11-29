de93e71713c540e3bd3aad8608ce3a39cd7414d5
M0hammed-18 <Mohammed.Alweheid.elq.js@tuwaiq.edu.sa>
2021-11-29 20:06:58 +0300

    FinshRegesterRouter

--- routers/controllers/user.js
+++ routers/controllers/user.js
@@ -2,4 +2,24 @@ const userModel=require("./../../db/models/user")
 const bcrypt=require("bcrypt")
 const jwt=require("jsonwebtoken")
 
+const SALT = Number(process.env.SALT);
+
+const regester= async (req,res)=>{
+    const { email, password, role } = req.body;
+    const saveEmail =email.toLowerCase();
+    const savePassword=await bcrypt.hash(password,SALT);
+    
+    const newUser=new userModel({
+        email:saveEmail,
+        password:savePassword,
+        role
+    })
+    newUser.save().then((result)=>{
+        res.json(result);
+    }).catch((err)=>{
+        res.json(err)
+    })
+}
+
+module.exports={regester}
 
